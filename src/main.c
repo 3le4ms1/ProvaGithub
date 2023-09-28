0be22e4ce5ca6d042dca5bbace816a88e235ff78
3le4ms1 <ismaeledegiorgi@gmail.com>
2023-09-28 09:40:32 +0200

    Adding PRINT_COORDS() to main.c

--- src/main.c
+++ src/main.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
 
+#define PRINT_COORDS()                          \
+  do{                                           \
+    printf("%s:%d\n", __FILE__, __LINE__);      \
+  }while(0);
+
 int main(void) {
+  PRINT_COORDS();
   printf("Hello, World");
   return 0;
 }
